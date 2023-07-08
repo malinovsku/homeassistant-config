fd6e6e49a428b19536e554c68f51f6fbcb36ce56
malinovsku <malinovsku94@gmail.com>
2023-07-08 23:26:31 +0300

    hyperion_set_image добавлен параметр priority

--- python_scripts/hyperion_set_image.py
+++ python_scripts/hyperion_set_image.py
@@ -8,6 +8,7 @@ data:
         file_path: Путь к картинке. Обязательно
         duration: Время задержки в ms установки картинки. Необязательно
         run_instance: Массив номеров instance, например [4] или [0,2]. Если не указано, то запсукается на всех. Необязательно
+        priority: Приоритет источника hyperion. По умолчанию 50. Необязательно
 
 '''
 import json
@@ -18,6 +19,7 @@ hyperion_host = "192.168.1.2:8090" # Хост и порт Hyperion
 file_path = data.get("file_path", False)
 duration = data.get("duration", -1)
 run_instance = data.get("run_instance", [-1])
+priority = data.get("priority", 50)
 encoded_string = None
 
 # Переводим картинку в формат base64
@@ -39,27 +41,27 @@ for inst in serverinfo["info"]["instance"]:
                     "instance" : num_instance}
         ws.send(json.dumps(data_i))
     if (inst["running"] and run_instance == [-1]) or (inst["running"] and num_instance in run_instance):
-        # Включаем LED Device
-        data_led = {"command":"componentstate",
-                    "componentstate":{
-                            "component":"LEDDEVICE",
-                            "state": True
-                            }
-                    }
-        ws.send(json.dumps(data_led))
         if file_path:
+            # Включаем LED Device
+            data_led = {"command":"componentstate",
+                        "componentstate":{
+                                "component":"LEDDEVICE",
+                                "state": True
+                                }
+                        }
+            ws.send(json.dumps(data_led))
             # Пакет для отправки картинки
             data_p = {"command": "image",
                     "imagedata": encoded_string, 
                     "name": "ha py script",
                     "format": "auto",
-                    "priority": 50,
+                    "priority": priority,
                     "duration": duration,
                     "origin": "HA py script"}
         else:
             # Пакет для очистки
             data_p = {"command":"clear",
-                    "priority":50}
+                    "priority":priority}
         ws.send(json.dumps(data_p))
 ws.close()
 
