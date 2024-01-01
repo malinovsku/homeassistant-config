8ea229cb33f6a30d84ead958e45aad3b996d156f
Dmitriy <34610364+malinovsku@users.noreply.github.com>
2024-01-02 02:16:18 +0300

    Update keenetic_backup.py

--- python_scripts/keenetic_backup.py
+++ python_scripts/keenetic_backup.py
@@ -1,10 +1,17 @@
 import requests
 import hashlib
 
-url = 'http://192.168.1.1'
-login="admin"
-passw="admin"
-folder_bk = "./"
+ip_default = "192.168.1.1"   # если не указан параметр "ip" при вызове скрипта
+folder_default = "./"   # если не указан параметр "folder_bk" при вызове скрипта
+login_default = "admin"     # если не указан параметр "login" при вызове скрипта
+passw_default = "admin"    # если не указан параметр "passw" при вызове скрипта
+name_file_default = "startup-config"    # если не указан параметр "name_file" при вызове скрипта
+
+url = f"http://{data.get('ip', ip_default)}"
+folder_bk = data.get('folder_bk', folder_default)
+login = data.get('login', login_default)
+passw = data.get('passw', passw_default)
+name_file = data.get('name_file', name_file_default)
 
 seskeen = requests.session()    
 response_ver = seskeen.get(f"{url}/auth")
@@ -13,8 +20,8 @@ md5 = hashlib.md5(md5.encode('utf-8'))
 sha = response_ver.headers["X-NDM-Challenge"] + md5.hexdigest()
 sha = hashlib.sha256(sha.encode('utf-8'))
 response_auth = seskeen.post(f"{url}/auth", json={"login": login, "password": sha.hexdigest()})
-response = seskeen.get(f"{url}/ci/startup-config")
+response_data = seskeen.get(f"{url}/ci/startup-config")
 seskeen.close()
 
-with open(f"{folder_bk}/startup-config.txt", "wb") as file_backup:
-    file_backup.write(response.content)
+with open(f"{folder_bk}/{name_file}.txt", "wb") as file_backup:
+    file_backup.write(response_data.content)
