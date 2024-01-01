5509dd7cd5a3777105f0a10831deffefceb84c02
Dmitriy <34610364+malinovsku@users.noreply.github.com>
2024-01-02 01:27:53 +0300

    Update keenetic_backup.py

--- python_scripts/keenetic_backup.py
+++ python_scripts/keenetic_backup.py
@@ -7,10 +7,10 @@ passw="admin"
 folder_bk = "./"
 
 seskeen = requests.session()    
-response = seskeen.get(f"{url}/auth")
-md5 = login + ":" + response.headers["X-NDM-Realm"] + ":" + passw
+response_ver = seskeen.get(f"{url}/auth")
+md5 = login + ":" + response_ver.headers["X-NDM-Realm"] + ":" + passw
 md5 = hashlib.md5(md5.encode('utf-8'))
-sha = response.headers["X-NDM-Challenge"] + md5.hexdigest()
+sha = response_ver.headers["X-NDM-Challenge"] + md5.hexdigest()
 sha = hashlib.sha256(sha.encode('utf-8'))
 response_auth = seskeen.post(f"{url}/auth", json={"login": login, "password": sha.hexdigest()})
 response = seskeen.get(f"{url}/ci/startup-config", json={"login": login, "password": sha.hexdigest()})
